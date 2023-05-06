4bbc84f584bfb5cd601c295d934294fa0445f6c5
JuDas69 <j.bohorquezb@uniandes.edu.co>
2023-05-06 18:09:02 -0500

    .

--- ProyectoHotel/ProyectoHotel/HOtel.java/src/GUI/InterfazMenuEmpleado.java
+++ ProyectoHotel/ProyectoHotel/HOtel.java/src/GUI/InterfazMenuEmpleado.java
@@ -11,19 +11,15 @@ public class InterfazMenuEmpleado extends JFrame{
 
         lbmenu = new JLabel();
 
-
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BorderLayout());
         mainPanel.add(lbmenu, BorderLayout.NORTH);
+        
 
         setTitle("Menu Empleados");
         setSize(1000, 750);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
 
-    }
-
-    public static void main(String args[]){
-        InterfazMenuEmpleado menu = new InterfazMenuEmpleado();
-        menu.inicializar();
     }
 }
