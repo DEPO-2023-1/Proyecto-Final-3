f8cee152f4d77f8d7142e66720377154c7750dec
Alejandro-Mariscal <a.mariscal@uniandes.edu.co>
2023-05-06 22:18:09 -0500

    MenuAdmin

--- ProyectoHotel/ProyectoHotel/HOtel.java/src/GUI/PanelSurAdmin.java
+++ ProyectoHotel/ProyectoHotel/HOtel.java/src/GUI/PanelSurAdmin.java
@@ -1,53 +1,62 @@
 package GUI;
 
-import javax.swing.JButton;
-import javax.swing.JPanel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
 public class PanelSurAdmin extends JPanel implements ActionListener{
-    private static final String archivos = "CARGAR INFO ARCHIVOS";
+	
+	private static final String archivos = "CARGAR INFO ARCHIVOS";
 	private static final String manual = "CARGAR INFO MANUAL";
-	private static final String actualizar = "ACTUALIZAR INFO ";
-	private static final String salir = "SALIR";
+	private static final String actualizar = "ACTUALIZARINFO";
+	private static final String salir = "sALIR";
+
 	
 	private JButton archivosButton;
 	private JButton manualButton;
 	private JButton actualizarButton;
 	private JButton salirButton;
 	
+	
 	public PanelSurAdmin() {
+		
 		setLayout(null);
 		
 		archivosButton = new JButton("CARGAR INFO ARCHIVOS");
-		archivosButton.setBounds(100, 100, 200, 50);
+		archivosButton.setBounds(150, 180, 150, 50);
 		archivosButton.addActionListener(this);
 		archivosButton.setActionCommand(archivos);
 		
-		manualButton = new JButton("CARGAR INFO MANUAL");
-		manualButton.setBounds(100, 200, 200, 50);
+		manualButton = new JButton("CARGAR ARCHIVOS MANUAL");
+		manualButton.setBounds(750, 180, 150, 50);
 		manualButton.addActionListener(this);
 		manualButton.setActionCommand(manual);
 		
 		actualizarButton = new JButton("ACTUALIZAR INFO");
-		actualizarButton.setBounds(100, 300, 200, 50);
+		actualizarButton.setBounds(450, 325, 150, 50);
 		actualizarButton.addActionListener(this);
 		actualizarButton.setActionCommand(actualizar);
 		
 		salirButton = new JButton("SALIR");
-		salirButton.setBounds(100, 400, 200, 50);
+		salirButton.setBounds(150, 480, 150, 50);
 		salirButton.addActionListener(this);
 		salirButton.setActionCommand(salir);
-		
+
 		
 		add(archivosButton);
 		add(manualButton);
 		add(actualizarButton);
 		add(salirButton);
+		
 	}
-
-    public void actionPerformed(ActionEvent e) {
-		String accion = e.getActionCommand();
-    
-}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String grito = e.getActionCommand();
+		
+		
+		
+	}
 }
