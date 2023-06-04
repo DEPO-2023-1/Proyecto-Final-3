370e537be065b8e13a759a8883e708fd7d8153cf
DaniCorredor <d.corredorl@unianes.edu.co>
2023-06-04 09:35:20 -0500

    otra restauracion

---
+++ ProyectoHotel/ProyectoHotel/HOtel.java/src/Clases/Reportes.java
@@ -0,0 +1,15 @@
+package Clases;
+
+import javax.swing.JFrame;  
+import javax.swing.SwingUtilities;  
+  /*
+import org.jfree.chart.ChartFactory;  
+import org.jfree.chart.ChartPanel;  
+import org.jfree.chart.JFreeChart;  
+import org.jfree.chart.plot.PlotOrientation;  
+import org.jfree.data.category.CategoryDataset;  
+import org.jfree.data.category.DefaultCategoryDataset;  
+*/
+public class Reportes {
+    
+}
