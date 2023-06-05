d1ac2b30f93e683b28fe0d3f5048eb0ae793d084
Alejandro-Mariscal <a.mariscal@uniandes.edu.co>
2023-06-04 21:09:59 -0500

    reportes completo

--- ProyectoHotel/ProyectoHotel/HOtel.java/src/Clases/Hotel.java
+++ ProyectoHotel/ProyectoHotel/HOtel.java/src/Clases/Hotel.java
@@ -1,5 +1,6 @@
 package Clases;
 import java.util.Date;
+import java.util.HashMap;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.util.ArrayList;
@@ -25,6 +26,8 @@ public class Hotel implements Serializable{
 	private boolean wifiGratis;
 	private boolean recepcion24h;
 	private boolean admiteMascotas;
+	private HashMap<String, Integer> consumidos;
+	private HashMap<String, Float> precios;
     
   //public static Frame frame;
 
@@ -42,7 +45,23 @@ public class Hotel implements Serializable{
 		this.wifiGratis = true;
 		this.recepcion24h = true;
 		this.admiteMascotas = true;
+		this.consumidos = new HashMap<String, Integer>();
+		this.precios = new HashMap<String, Float>();
     }
+    
+   public void Mapa1(ArrayList<Inventario> inventarios) {
+	  for(Inventario i: inventarios) {
+		  String nombre = i.getProducto();
+		  this.consumidos.put(nombre, 0);
+	  }
+   }
+   public void Mapa2(ArrayList<MenuRestaurante> productos) {
+	   for(MenuRestaurante p: productos) {
+		   String nombre = p.getNombre();
+		   float precio =p.getPrecio();
+		   this.precios.put(nombre, precio);
+	   }
+   }
 
     public boolean seleccionarUsuario(String login, String contraseña, int usuario){
 		String direccion = "";
@@ -102,6 +121,8 @@ public class Hotel implements Serializable{
                 i.setCantidad(valor);
                 ConsumoHot servicio1=new ConsumoHot(IDHabitacion, "consumo",servicio);
                 consumosHotel.add(servicio1);
+                int consume = consumidos.get(i.getProducto()+1);
+                consumidos.put(i.getProducto(), consume);
                 if(i.getCantidad()==0){
                     inventarios.remove(i);
                 }
@@ -731,7 +752,7 @@ public class Hotel implements Serializable{
 			linea = lector.readLine();
 		}
 		lector.close();
-    	
+    	Mapa1(inventarios);
     }
     
     private void cargarServicio(String Servicio) throws IOException{
@@ -785,6 +806,7 @@ public class Hotel implements Serializable{
 			}
 
 			linea = lector.readLine();
+			Mapa2(productos);
 		}
 		lector.close();
 	}
@@ -894,6 +916,13 @@ public String input(String mensaje)
 	public ArrayList<MenuRestaurante> getProductos() {
 		return productos;
 	}
+	public HashMap<String, Integer> getMapaCantidad() {
+		return consumidos;
+	}
+	public HashMap<String, Float> getMapaPrecios(){
+		return precios;
+	}
+	
 
 
 
